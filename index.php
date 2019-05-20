9da6141b6dcf27354a9e08e478bcf287e6091c35
denis dudin <denis.dudin@brainysoft.ru>
2019-05-20 18:52:58 +0300

    js scram client in progress

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ use Bs\Sdk\Auth\Encryptors\PhpHash;
 use Bs\Sdk\Auth\Strategy\AuthStrategyDispatcher;
 use Bs\Sdk\Auth\Strategy\RandomString;
 
-/* 1. Сначала сделаем handShake. Отправим на сервер логин */
+/* 1. Сначала сделаем handShake. Отправим логин и clientNonce на сервер */
 
 $user_login = 'admin_scram_sha1';
 $user_password = '123zЯ';
@@ -26,15 +26,11 @@ $user_password = '123zЯ';
 $customer_key = 'qa';
 $client = new GuzzleHttp\Client();
 
-
 $clientNonce = (new RandomString())->handle();
-
 
 //$clientNonce = "A0394B2F298F03699B97A3BD29ADCB03C375ECDD";
 //$serverNonce = "9C41A03630A1AC28174401E834E21BBA2EA523D7";
 //$salt = '260C152FD22082DB5E875E53994CAE750B98AC372B06C516';
-
-pp($clientNonce);
 
 $response = $client->request('POST', 'http://172.16.10.62:8082/bs-core/auth/first-message', [
     'headers' => [
@@ -57,21 +53,21 @@ pre($arr);
 $dispatcher = new AuthStrategyDispatcher();
 
 
-$params['hashAlg'] = explode('_',$arr['data']['authMode'])[1];
-$params['protocolVersion'] = explode('_',$arr['data']['authMode'])[0];
+$params['hashAlg'] = explode('_', $arr['data']['authMode'])[1];
+$params['protocolVersion'] = explode('_', $arr['data']['authMode'])[0];
 $params['serverNonce'] = $arr['data']['serverNonce'];//'D1CDFD86BDC80C9ABE5BE3835B13DCCB4AF7A453'
 $params['encryptedServerNonce'] = $arr['data']['encryptedServerNonce'];
 $params['clientNonce'] = $clientNonce;
 $params['iterationCount'] = $arr['data']['iterationCount'];
 $params['salt'] = $arr['data']['salt'];
 
-pre('$params[\'hashAlg\']: '.$params['hashAlg']);
+pre('$params[\'hashAlg\']: ' . $params['hashAlg']);
 
 $strategy = $dispatcher->resolveStrategy($params);
 
 $data = array_map('trim', $_REQUEST);
-if (!empty($data['encryptor'])){ //phphash or openssl
-    print_r('encryptor : '.$data['encryptor'].'<br>'.PHP_EOL );
+if (!empty($data['encryptor'])) { //phphash or openssl
+    print_r('encryptor : ' . $data['encryptor'] . '<br>' . PHP_EOL);
 } else throw new Exception('<<Encryptor not setted>>.');
 
 switch ($data['encryptor']) {
@@ -82,16 +78,15 @@ switch ($data['encryptor']) {
         $strategy->setEncryptor(new PhpHash());
         break;
     default:
-        throw new \RuntimeException('Error: Bad $encryptor version: '. $data['encryptor']);
+        throw new \RuntimeException('Error: Bad $encryptor version: ' . $data['encryptor']);
 }
 
 /* 2.1 Сгенерируем client proof */
 $client_proof = $strategy->createClientProof($user_password);
-pre('$client_proof: '.$client_proof.PHP_EOL);
+pre('$client_proof: ' . $client_proof . PHP_EOL);
 //$client_proof = bin2hex($client_proof);
 //pre('bin2hex($client_proof): '.$client_proof.PHP_EOL);
 /* 3 отправим серверу CP На проверку */
-
 
 
 $request = [
@@ -103,7 +98,7 @@ $request = [
     "authMode" => $arr['data']['authMode']
 ];
 
-pre('request: '.json_encode($request, 128 + 256));
+pre('request: ' . json_encode($request, 128 + 256));
 
 try {
     $response = $client->request('POST', 'http://172.16.10.62:8082/bs-core/auth/final-message', [
@@ -118,34 +113,23 @@ try {
     pre($arr);
 
 } catch (\GuzzleHttp\Exception\RequestException $e) {
-    pre('failed response body: '.json_encode(json_decode($e->getResponse()->getBody()), 128 + 256));
+    pre('failed response body: ' . json_encode(json_decode($e->getResponse()->getBody()), 128 + 256));
 }
 
 
-
-function pre($arr){
+function pre($arr)
+{
     echo '<pre>';
-    echo print_r($arr,1);
+    echo print_r($arr, 1);
     echo '</pre>';
 }
 
-function String2Hex($string){
+function String2Hex($string)
+{
     return implode(unpack("H*", $string));
 }
 
-function hexToStr($hex){//должно работать так же как и hex2bin ( string $data ) : string
+function hexToStr($hex)
+{//должно работать так же как и hex2bin ( string $data ) : string
     return pack("H*", $hex);
-}
-
-
-function pp($var){
-    function print_var_name($var) {
-        foreach($GLOBALS as $var_name => $value) {
-            if ($value === $var) {
-                return $var_name;
-            }
-        }
-        return false;
-    }
-    return print_r(print_var_name($var). ': '.$var.PHP_EOL);
 }
--- src/Strategy/ScramAuthStrategy.php
+++ src/Strategy/ScramAuthStrategy.php
@@ -16,7 +16,7 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     /**
      * @return mixed
      */
-    public function getClientNonce()
+    private function getClientNonce()
     {
         return $this->clientNonce;
     }
@@ -32,7 +32,7 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     /**
      * @return mixed
      */
-    public function getSalt()
+    private function getSalt()
     {
         return $this->salt;
     }
@@ -81,7 +81,7 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     /**
      * @return mixed
      */
-    public function getHashAlg()
+    private function getHashAlg()
     {
         return $this->hashAlg;
     }
@@ -89,7 +89,7 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     /**
      * @return mixed
      */
-    public function getIterationCount()
+    private function getIterationCount()
     {
         return $this->iterationCount;
     }
@@ -97,7 +97,7 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     /**
      * @return mixed
      */
-    public function getServerNonce()
+    private function getServerNonce()
     {
         return $this->serverNonce;
     }
@@ -106,59 +106,18 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     public function createClientProof($password)
     {
         $salt = $this->getSalt();
-
         $i = $this->getiterationCount();
-
         $algo = $this->getHashAlg();
         $clientNonce = $this->getClientNonce();
         $serverNonce = $this->getServerNonce();
 
+
         $saltedPassword = $this->hi($password, $salt, $i);
-
-        $clientKey = hash_hmac($algo, "",$saltedPassword);
-        pre('password: '.$password);
-        pre('$saltedPassword: '.bin2hex($saltedPassword).'<br>');
-        pre('$clientKey: '.$clientKey.'<br>');
-
-
+        $clientKey = hash_hmac($algo, "",$saltedPassword,0);
         $storedKey = $this->compute(hex2bin($clientKey),1 );
         $authMessage = $clientNonce . $serverNonce;
-
-        $clientSignature = hash_hmac($algo, hex2bin($authMessage),$storedKey, TRUE);
-
+        $clientSignature = hash_hmac($algo, hex2bin($authMessage),$storedKey, 1);
         $clientProof = hex2bin($clientKey) ^ $clientSignature;
-
-
-        pre('$storedKey: '.$storedKey.'<br>');
-        pre('$authMessage: '.$authMessage.'<br>');
-        pre('$clientSignature: '.bin2hex($clientSignature).'<br>');
-        pre('$clientProof: '.bin2hex($clientProof).'<br>');
-        pre('$salt: '.$salt.'<br>');
-        pre('$i: '.$i.'<br>');
-        pre('$algo: '.$algo.'<br>');
-        pre('$clientNonce: '.$clientNonce.'<br>');
-        pre('$serverNonce: '.$serverNonce.'<br>');
-
-
-
-        pre('hex2bin($authMessage): '.hex2bin($authMessage).'<br>');
-
-
-        pre('bin2hex($clientProof): '.strtoupper (bin2hex($clientProof)).'<br>');
-
-//        print_r('$salt: '.$salt.PHP_EOL);
-//        print_r('$i: '.$i.PHP_EOL);
-//        print_r('$algo: '.$algo.PHP_EOL);
-//        print_r('$clientNonce: '.$clientNonce.PHP_EOL);
-//        print_r('$serverNonce: '.$serverNonce.PHP_EOL);
-//        print_r('$saltedPassword: '.$saltedPassword.PHP_EOL);
-//        print_r('$clientKey: '.$clientKey.PHP_EOL);
-//        print_r('$storedKey: '.$storedKey.PHP_EOL);
-//        print_r('$authMessage: '.$authMessage.PHP_EOL);
-//        print_r('$clientSignature: '.$clientSignature.PHP_EOL);
-//        print_r('$clientProof: '.$clientProof.PHP_EOL);
-//        print_r('bin2hex($clientProof): '.strtoupper (bin2hex($clientProof)).PHP_EOL);
-
 
         return strtoupper (bin2hex($clientProof));
     }
@@ -170,7 +129,7 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
 
     /**
      * Hi() call, which is essentially PBKDF2 (RFC-2898) with HMAC-H() as the pseudorandom function.
-     * @example : return hash_pbkdf2($algo, $data, hex2bin($key), $i);
+     * PBKDF2 example : return hash_pbkdf2($algo, $data, hex2bin($key), $i);
      * @param string $str The string to hash.
      * @param string $hash The hash value.
      * @param int $i The iteration count.
@@ -182,24 +141,19 @@ class ScramAuthStrategy extends AbstractAuthStrategy implements IAuthStrategy
     {
         $algo = $this->getHashAlg();
         $int1 = "\0\0\0\1";
-//        pre('data: '.$data);
-//        pre('key: '.$key);
         $salt_int = $key.bin2hex($int1);
-        //$salt_int = '260C152FD22082DB5E875E53994CAE750B98AC372B06C51600000001';
-
-        //print_r('$salt_int: '.$key.bin2hex($int1).'<br>');
-        $ui = hash_hmac($algo, hex2bin($salt_int), $data,1);//base_convert($salt_int,16,2)
-        //print_r('u1: '.bin2hex($ui).'<br>');
+        $ui = hash_hmac($algo, hex2bin($salt_int), $data,1);
         $result = $ui;
         for ($k = 1; $k < $i; $k++)
         {
             $ui = hash_hmac($algo,$ui,$data,1);
+
             $result = $result ^ $ui;
-            //echo "u",$k+1,": ".bin2hex($ui).'<br>';
-            //echo "result",$k+1,": ".bin2hex($result).'<br>';
-            //if ($k>5) die();
+
+//            echo  "bin2hex(u", $k + 1, "): " . bin2hex($ui).'<br>';
+//            echo  "bin2hex(result", $k + 1, "): " . bin2hex($result).'<br>';
+//            if ($k > 5) break;
         }
         return $result;
-        //return hash_pbkdf2($algo, $data, hex2bin($key), $i);
     }
 }
